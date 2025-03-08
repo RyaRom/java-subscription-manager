ba12d9425a1b6e85cce3e8f00a915994ab258474
nyadesu <riabikin.roman@gmail.com>
2025-03-09 00:04:01 +0300

    feat: bot commands config in application.yaml

--- bot/src/main/java/backend/academy/bot/config/BotConfig.java
+++ bot/src/main/java/backend/academy/bot/config/BotConfig.java
@@ -4,6 +4,8 @@ import backend.academy.bot.telegram.utils.filters.UpdateProcessor;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import jakarta.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -13,39 +15,75 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record BotConfig(@NotEmpty String telegramToken, @NotEmpty String helpMessage, @NotEmpty String scrapperUrl) {
+public record BotConfig(
+    @NotEmpty String telegramToken,
+    @NotEmpty String scrapperUrl,
+    BotCommands settings
+) {
 
     @Bean
     @Qualifier("helpMessage")
     public String helpMessage() {
-        return helpMessage;
+        return settings.commands
+            .stream()
+            .map(BotCommand::toString)
+            .collect(Collectors.joining("\n"));
+    }
+
+    @Bean
+    public BotCommands botCommands() {
+        return settings;
     }
 
     @Bean
     @Qualifier("scrapperHttpClient")
     public WebClient scrapperHttpClient() {
         return WebClient.builder()
-                .baseUrl(scrapperUrl)
-                .defaultHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-                .build();
+            .baseUrl(scrapperUrl)
+            .defaultHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+            .build();
+    }
+
+    @Bean
+    @Qualifier("tgHttpClient")
+    public WebClient botHttpClient() {
+        return WebClient.builder()
+            .baseUrl("https://api.telegram.org/bot" + telegramToken)
+            .defaultHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+            .build();
     }
 
     @Bean
     public TelegramBot telegramBot(UpdateProcessor updateProcessor) {
         var telegramBot = new TelegramBot(telegramToken);
         telegramBot.setUpdatesListener(
-                updates -> {
-                    updates.forEach(updateProcessor::consumeUpdate);
-                    return UpdatesListener.CONFIRMED_UPDATES_ALL;
-                },
-                e -> {
-                    if (e.response() != null) {
-                        e.response().errorCode();
-                        e.response().description();
-                    } else {
-                        e.printStackTrace();
-                    }
-                });
+            updates -> {
+                updates.forEach(updateProcessor::consumeUpdate);
+                return UpdatesListener.CONFIRMED_UPDATES_ALL;
+            },
+            e -> {
+                if (e.response() != null) {
+                    e.response().errorCode();
+                    e.response().description();
+                } else {
+                    e.printStackTrace();
+                }
+            });
         return telegramBot;
+    }
+
+    public record BotCommands(
+        List<BotCommand> commands
+    ) {
+    }
+
+    public record BotCommand(
+        String command,
+        String description
+    ) {
+        @Override
+        public String toString() {
+            return command + " - " + description;
+        }
     }
 }
