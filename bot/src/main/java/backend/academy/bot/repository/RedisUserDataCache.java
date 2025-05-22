80207637291fe8c2a8915389319df6374cd16fed
riabirom <riabikinrom@yandex.ru>
2025-05-23 02:27:14 +0300

    fix NPE in bot filters parse

--- bot/src/main/java/backend/academy/bot/repository/RedisUserDataCache.java
+++ bot/src/main/java/backend/academy/bot/repository/RedisUserDataCache.java
@@ -5,6 +5,7 @@ import backend.academy.bot.telegram.sdk.fsm.BotState;
 import io.lettuce.core.KeyValue;
 import io.lettuce.core.Value;
 import io.lettuce.core.api.reactive.RedisReactiveCommands;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
@@ -15,8 +16,8 @@ public class RedisUserDataCache implements UserDataCacheRepository {
     @Override
     public Mono<Void> updateState(Long chatId, BotState state) {
         return redisReactiveCommandsString
-                .hset(prefix(chatId), "state", state.toString())
-                .then();
+            .hset(prefix(chatId), "state", state.toString())
+            .then();
     }
 
     @Override
@@ -27,8 +28,8 @@ public class RedisUserDataCache implements UserDataCacheRepository {
     @Override
     public Mono<Void> updateFilters(Long chatId, String filters) {
         return redisReactiveCommandsString
-                .hset(prefix(chatId), "filters", filters)
-                .then();
+            .hset(prefix(chatId), "filters", filters)
+            .then();
     }
 
     @Override
@@ -39,13 +40,17 @@ public class RedisUserDataCache implements UserDataCacheRepository {
     @Override
     public Mono<UserCache> getUser(Long chatId) {
         return redisReactiveCommandsString
-                .hgetall(prefix(chatId))
-                .collectMap(KeyValue::getKey, Value::getValue)
-                .map(map -> new UserCache(
-                        SubscriptionBotState.valueOf(map.get("state")),
-                        map.get("link"),
-                        map.get("tags"),
-                        map.get("filters")));
+            .hgetall(prefix(chatId))
+            .collectMap(KeyValue::getKey, Value::getValue)
+            .map(map -> UserCache.builder()
+                .filters(map.get("filters") == null ? "" : map.get("filters"))
+                .tags(map.get("tags") == null ? "" : map.get("tags"))
+                .botState(Optional.ofNullable(map.get("state"))
+                    .map(SubscriptionBotState::valueOf)
+                    .orElse(null))
+                .link(map.get("link") == null ? "" : map.get("link"))
+                .build()
+            );
     }
 
     @Override
